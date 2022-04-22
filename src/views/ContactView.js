de00bf2369597a4016a71fbe1f5b38d9c5cc4184
Efsane <efs.gun@gmail.com>
2022-04-22 13:53:56 +0300

    added contact views

--- src/views/ContactView.js
+++ src/views/ContactView.js
@@ -1,11 +1,45 @@
 import React  from "react";
 import Layout from "../components/partials/layout";
+import {AiOutlineMail} from "react-icons/ai";
+import {FiGithub, FiInstagram} from "react-icons/fi";
 
+import {RiLinkedinLine} from "react-icons/ri";
 function ContactView() {
     return (
         <Layout>
-            <div>
-               Contact View
+            <div className="pt-20">
+                <h1 className="text-center text-5xl font-bold  mb-20 text-primary-orange">İletişim
+                    <span className="title-bg ">İletişim</span>
+                </h1>
+                <div className="container flex flex-col items-center ">
+
+                    <a href="mailto:eraybulutlar@gmail.com" target="_blank">
+                        <div className="flex hover:text-primary-orange mb-10 items-center cursor-pointer">
+                            <AiOutlineMail className="font-bold text-3xl    "/>
+                            <p className="pl-5">eraybulutlar@gmail.com</p>
+                        </div>
+                    </a>
+
+                    <a
+                        href="https://tr.linkedin.com/in/eray-bulut-255644196"
+                        target="_blank"
+                    >
+                        <div className="flex mb-10 hover:text-primary-orange items-center ">
+                            <RiLinkedinLine className="font-bold text-3xl" />
+                            <p className="pl-5"> eraybulut</p>
+                        </div>
+                    </a>
+
+
+
+                    <a href="https://github.com/eraybulut">
+                        <div className="flex mb-10 hover:text-primary-orange items-center ">
+                            <FiGithub className="font-bold text-3xl  cursor-pointer  "/>
+                            <p className="pl-5">eraybulut</p>
+                        </div>
+                    </a>
+                </div>
+
             </div>
         </Layout>
     );
